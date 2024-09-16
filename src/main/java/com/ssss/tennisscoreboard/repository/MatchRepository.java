c7115758a61282da06ed1b41733d556c828afbbf
ssss1131 <sauletmardan20@gmail.com>
2024-09-16 21:11:49 +0500

    feat: new methods for finding filtered limited rows from database

--- src/main/java/com/ssss/tennisscoreboard/repository/MatchRepository.java
+++ src/main/java/com/ssss/tennisscoreboard/repository/MatchRepository.java
@@ -1,11 +1,52 @@
 package com.ssss.tennisscoreboard.repository;
 
 import com.ssss.tennisscoreboard.entity.Match;
-import jakarta.persistence.EntityManager;
+import jakarta.persistence.*;
+import org.hibernate.graph.GraphSemantic;
 
-public class MatchRepository extends BaseRepository<Long, Match>{
+import java.util.List;
+import java.util.Map;
+
+public class MatchRepository extends BaseRepository<Long, Match> {
+
+    private static final String FIND_ALL = "SELECT m FROM Match m";
+
+    private static final String FIND_WITH_FILTERS = FIND_ALL +
+                                                    " WHERE m.player1.name LIKE :filter " +
+                                                    "OR m.player2.name LIKE :filter ";
+
 
     public MatchRepository(EntityManager entityManager) {
         super(Match.class, entityManager);
     }
+
+    public List<Match> findFilteredMatches(String filter, int pageSize, int page) {
+        String filterPattern = "%" + filter + "%";
+        EntityGraph<Match> graph = getEntityManager().createEntityGraph(Match.class);
+        graph.addAttributeNodes("player1", "player2", "winner");
+        return getEntityManager().createQuery(FIND_WITH_FILTERS, Match.class)
+                .setHint(GraphSemantic.LOAD.getJakartaHintName(), graph)
+                .setParameter("filter", filterPattern)
+                .setFirstResult(page * pageSize)
+                .setMaxResults(pageSize)
+                .getResultList();
+    }
+
+    public List<Match> findFilteredMatches(int pageSize, int page) {
+        EntityGraph<Match> graph = getEntityManager().createEntityGraph(Match.class);
+        graph.addAttributeNodes("player1", "player2", "winner");
+        return getEntityManager().createQuery(FIND_ALL, Match.class)
+                .setHint(GraphSemantic.LOAD.getJakartaHintName(), graph)
+                .setFirstResult(page * pageSize)
+                .setMaxResults(pageSize)
+                .getResultList();
+    }
+
+
+    public List<Match> findAll() {
+        return getEntityManager()
+                .createQuery(FIND_ALL, Match.class)
+                .getResultList();
+    }
+
 }
