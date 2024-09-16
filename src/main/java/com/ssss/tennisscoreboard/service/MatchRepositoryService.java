0afbfb246746815c13986120df2e720fcd2431cb
ssss1131 <sauletmardan20@gmail.com>
2024-09-16 21:12:49 +0500

    feat: services for filter and getting pagination size that depends to PAGE_SIZE from listener

--- src/main/java/com/ssss/tennisscoreboard/service/MatchRepositoryService.java
+++ src/main/java/com/ssss/tennisscoreboard/service/MatchRepositoryService.java
@@ -6,18 +6,44 @@ import com.ssss.tennisscoreboard.util.HibernateUtils;
 import lombok.Cleanup;
 import org.hibernate.Session;
 
+import java.util.List;
+
 public class MatchRepositoryService {
 
     private MatchRepository matchRepository;
 
-    //TODO мб передавать boolean чтобы если что не удалять из памяти там
-    public Match createNewMatch(Match match){
-        @Cleanup Session session = HibernateUtils.getSessionFactory().getCurrentSession();
+    //TODO попробовать убрать Cleanup ну почитать короч можно ли
+    public Match createNewMatch(Match match) {
+        Session session = HibernateUtils.getSessionFactory().getCurrentSession();
         session.beginTransaction();
-        MatchRepository matchRepository = new MatchRepository(session);
+        matchRepository = new MatchRepository(session);
         matchRepository.save(match);
         session.getTransaction().commit();
         return match;
     }
 
+    public List<Match> getMatchesWithFilter(String filter, int pageSize, int page) {
+        Session session = HibernateUtils.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        matchRepository = new MatchRepository(session);
+        List<Match> matches;
+        page = page - 1;
+        if (filter != null && !filter.trim().isEmpty()) {
+            matches = matchRepository.findFilteredMatches(filter, pageSize, page);
+        } else {
+            matches = matchRepository.findFilteredMatches(pageSize, page);
+        }
+        session.getTransaction().commit();
+
+        return matches;
+    }
+
+    public int getAllPagesCountWithPageSize(int pageSize) {
+        Session session = HibernateUtils.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        matchRepository = new MatchRepository(session);
+        List<Match> matches = matchRepository.findAll();
+        session.getTransaction().commit();
+        return (matches.size() + pageSize - 1) / pageSize;
+    }
 }
