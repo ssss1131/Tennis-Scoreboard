5c44c694d3477f24dc212b2de854bbe72e9c53ca
ssss1131 <sauletmardan20@gmail.com>
2024-09-16 21:09:04 +0500

    feat:not fully but mostly completed doGet method
    with logic of giving pagination size of page. Giving all matches that corresponds to filter or without it.Only have bags like if he filter the pages would continue to show all matches not rows that correspond to filter

--- src/main/java/com/ssss/tennisscoreboard/controller/AllMatchesController.java
+++ src/main/java/com/ssss/tennisscoreboard/controller/AllMatchesController.java
@@ -1,6 +1,9 @@
 package com.ssss.tennisscoreboard.controller;
 
+import com.ssss.tennisscoreboard.entity.Match;
+import com.ssss.tennisscoreboard.service.MatchRepositoryService;
 import com.ssss.tennisscoreboard.util.JspPathFinder;
+import com.ssss.tennisscoreboard.util.UserInputValidator;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,12 +11,35 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/matches")
 public class AllMatchesController extends HttpServlet {
 
+    private MatchRepositoryService matchRepositoryService;
+    private int pageSize;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        matchRepositoryService = new MatchRepositoryService();
+        Object pageSizeAttr = getServletContext().getAttribute("pageSize");
+        if (pageSizeAttr == null) {
+            throw new ServletException("PageSize attribute is not initialized in ServletContext");
+        }
+        pageSize = (int) pageSizeAttr;
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //TODO надо сделать так чтобы pages передавал не только общее ну и количество при фильтре если есть
+        String maybePage = req.getParameter("page");
+        String maybeFilter = req.getParameter("query");
+        int page = maybePage != null ? UserInputValidator.validate(maybePage) : 1;
+        List<Match> matches = matchRepositoryService.getMatchesWithFilter(maybeFilter, pageSize, page);
+        int allPages = matchRepositoryService.getAllPagesCountWithPageSize(pageSize);
+        req.setAttribute("matches", matches);
+        req.setAttribute("pages", allPages);
         req.getRequestDispatcher(JspPathFinder.getPath("AllMatches")).forward(req, resp);
     }
 }
