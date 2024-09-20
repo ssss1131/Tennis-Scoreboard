569618ada5baf24bf4234ce03d5b763bd6203c87
ssss1131 <sauletmardan20@gmail.com>
2024-09-20 20:19:19 +0500

    feat: exception occur if same name occur

---
+++ src/main/java/com/ssss/tennisscoreboard/exception/SameNameException.java
@@ -0,0 +1,7 @@
+package com.ssss.tennisscoreboard.exception;
+
+public class SameNameException extends RuntimeException {
+    public SameNameException(String message) {
+        super(message);
+    }
+}
