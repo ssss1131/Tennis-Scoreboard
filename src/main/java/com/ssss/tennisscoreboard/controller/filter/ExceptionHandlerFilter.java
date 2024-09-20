c889a8fe8ecc6cc6b271bad99967d48cd2d71156
ssss1131 <sauletmardan20@gmail.com>
2024-09-20 20:20:34 +0500

    feat: Exception handling and redirecting to new-match page

---
+++ src/main/java/com/ssss/tennisscoreboard/controller/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,27 @@
+package com.ssss.tennisscoreboard.controller.filter;
+
+
+import com.ssss.tennisscoreboard.exception.SameNameException;
+import com.ssss.tennisscoreboard.util.JspPathFinder;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class ExceptionHandlerFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        try{
+            chain.doFilter(req, res);
+        }catch (SameNameException e){
+            req.setAttribute("error", e.getMessage());
+            req.getRequestDispatcher(JspPathFinder.getPath("NewMatch")).forward(req, res);
+        }
+    }
+}
