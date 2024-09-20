35b8cc9f4896730d9e448be27aa523213fd23e2e
ssss1131 <sauletmardan20@gmail.com>
2024-09-20 16:45:25 +0500

    feat: validation of filter attribute

--- src/main/java/com/ssss/tennisscoreboard/util/UserInputValidator.java
+++ src/main/java/com/ssss/tennisscoreboard/util/UserInputValidator.java
@@ -2,16 +2,26 @@ package com.ssss.tennisscoreboard.util;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.Optional;
+
 @UtilityClass
 public class UserInputValidator {
 
-    public static int validate(String maybePage) {
+    public static int validatePage(String maybePage) {
         int page;
         try{
-            page = Integer.parseInt(maybePage);
+            page = Integer.parseInt(maybePage) - 1;
         } catch (NumberFormatException e) {
-            page = 1;
+            page = 0;
         }
         return page;
+    }
+
+    public static Optional<String> validateFilter(String maybeFilter){
+        if(maybeFilter == null || maybeFilter.isEmpty()){
+           return Optional.empty();
+        }
+        String filter = maybeFilter.trim();
+        return Optional.of(filter);
     }
 }
