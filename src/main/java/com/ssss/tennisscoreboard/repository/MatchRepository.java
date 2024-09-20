c5aca0253bb08674ab62651d4b06088bcfa66828
ssss1131 <sauletmardan20@gmail.com>
2024-09-20 16:41:50 +0500

    feat,refactor: new feature is counting quantity of filtered pages and refactored findAll bcs matches not needed

--- src/main/java/com/ssss/tennisscoreboard/repository/MatchRepository.java
+++ src/main/java/com/ssss/tennisscoreboard/repository/MatchRepository.java
@@ -5,16 +5,19 @@ import jakarta.persistence.*;
 import org.hibernate.graph.GraphSemantic;
 
 import java.util.List;
-import java.util.Map;
 
 public class MatchRepository extends BaseRepository<Long, Match> {
 
     private static final String FIND_ALL = "SELECT m FROM Match m";
 
-    private static final String FIND_WITH_FILTERS = FIND_ALL +
-                                                    " WHERE m.player1.name LIKE :filter " +
-                                                    "OR m.player2.name LIKE :filter ";
+    private static final String FILTER_BY_NAMES = " WHERE m.player1.name LIKE :filter " +
+                                                  "OR m.player2.name LIKE :filter";
 
+    private static final String FIND_WITH_FILTERS = FIND_ALL + FILTER_BY_NAMES;
+
+    private static final String FIND_COUNT_ALL = "SELECT COUNT(m) FROM Match m ";
+
+    private static final String FIND_COUNT_WITH_FILTERS = FIND_COUNT_ALL + FILTER_BY_NAMES;
 
     public MatchRepository(EntityManager entityManager) {
         super(Match.class, entityManager);
@@ -42,11 +45,22 @@ public class MatchRepository extends BaseRepository<Long, Match> {
                 .getResultList();
     }
 
-
-    public List<Match> findAll() {
+    public int findCountOfFilteredPlayers(String filter){
+        String filterPattern = "%" + filter + "%";
         return getEntityManager()
-                .createQuery(FIND_ALL, Match.class)
-                .getResultList();
+                .createQuery(FIND_COUNT_WITH_FILTERS, Long.class)
+                .setParameter("filter", filterPattern)
+                .getSingleResult()
+                .intValue();
+    }
+
+
+    public int findCountOfAll() {
+        return getEntityManager()
+                .createQuery(FIND_COUNT_ALL , Long.class)
+                .getSingleResult()
+                .intValue();
+
     }
 
 }
