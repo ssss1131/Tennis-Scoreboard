c12b985f37fe532191fe7d77a36516c404bfb52e
ssss1131 <sauletmardan20@gmail.com>
2024-09-21 13:49:24 +0500

    refactor: renamed class and method

--- src/main/java/com/ssss/tennisscoreboard/util/MapperUtils.java
+++ src/main/java/com/ssss/tennisscoreboard/util/MapperUtils.java
@@ -1,22 +1,21 @@
-package com.ssss.tennisscoreboard.mapper;
+package com.ssss.tennisscoreboard.util;
 
 import com.ssss.tennisscoreboard.dto.TennisPlayerMatchInfo;
-import com.ssss.tennisscoreboard.entity.Player;
+import com.ssss.tennisscoreboard.model.Player;
 import lombok.experimental.UtilityClass;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 
 @UtilityClass
-public class ToPlayerMapper {
+public class MapperUtils {
     private static final ModelMapper modelMapper = new ModelMapper();
 
     static {
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
     }
 
-    public static Player mapToPlayer(TennisPlayerMatchInfo playerMatchInfo){
+    public static Player mapTo(TennisPlayerMatchInfo playerMatchInfo){
         return modelMapper.map(playerMatchInfo, Player.class);
     }
-
 
 }
