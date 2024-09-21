cdadec810aa269861d6d5285157a7d9d261825b4
ssss1131 <sauletmardan20@gmail.com>
2024-09-21 13:51:33 +0500

    refactor: removed TODO bcs already done

--- src/main/java/com/ssss/tennisscoreboard/model/score/Score.java
+++ src/main/java/com/ssss/tennisscoreboard/model/score/Score.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 @Data
 public class Score {
-    //TODO по сути надо чекнуть как тут по дефолту 0 сделать хотя вроде так и должно быть но там null странно
     private int sets;
 
     private int games;
