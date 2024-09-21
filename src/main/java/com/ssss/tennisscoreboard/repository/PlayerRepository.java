4901f94619486de700e10fc185d97f8aae7b4cc1
ssss1131 <sauletmardan20@gmail.com>
2024-09-21 13:53:06 +0500

    refactor: removed isNotExist method and made returning value Optional because may be null

--- src/main/java/com/ssss/tennisscoreboard/repository/PlayerRepository.java
+++ src/main/java/com/ssss/tennisscoreboard/repository/PlayerRepository.java
@@ -1,11 +1,13 @@
 package com.ssss.tennisscoreboard.repository;
 
-import com.ssss.tennisscoreboard.entity.Player;
+import com.ssss.tennisscoreboard.model.Player;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
+
+import java.util.Optional;
 
 public class PlayerRepository extends BaseRepository<Long, Player> {
 
@@ -16,18 +18,14 @@ public class PlayerRepository extends BaseRepository<Long, Player> {
         cb = getEntityManager().getCriteriaBuilder();
     }
 
-    public boolean isNotExist(String name){
-        return findByName(name) == null;
-    }
-
-    public Player findByName(String name){
+    public Optional<Player> findByName(String name){
         CriteriaQuery<Player> criteria = cb.createQuery(Player.class);
         Root<Player> player = criteria.from(Player.class);
         criteria.where(cb.equal(player.get("name"), name));
         try {
-            return getEntityManager().createQuery(criteria).getSingleResult();
+            return Optional.of(getEntityManager().createQuery(criteria).getSingleResult());
         } catch (NoResultException e) {
-            return null;
+            return Optional.empty();
         }
 
     }
